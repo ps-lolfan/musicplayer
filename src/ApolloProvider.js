07ce1a6fc44b0204aa48e22eaa1b76f3ec4efa9d
Praveen Sinnur <praveen@zeliot.in>
2021-04-06 12:07:18 +0530

    db connection done successfully

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://aqueous-mesa-26130.herokuapp.com/",
+  uri: process.env.REACT_APP_ENDPOINT,
 });
 
 const authLink = setContext(() => {
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import Player from "./components/Player";
-import { useQuery } from "@apollo/react-hooks";
+// import { useQuery } from "@apollo/react-hooks";
 
-import { FETCH_ALL_SONGS } from "./utils/graphql";
+// import { FETCH_ALL_SONGS } from "./utils/graphql";
 function App() {
-  const { loading, data } = useQuery(FETCH_ALL_SONGS);
+  //   const { loading, data } = useQuery(FETCH_ALL_SONGS);
 
-  if (data) {
-    console.log("all songs", data.getSongs);
-  }
+  //   if (data) {
+  //     console.log("all songs", data.getSongs);
+  //   }
 
   const [songs] = useState([
     {
